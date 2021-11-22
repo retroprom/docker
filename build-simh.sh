9b44a35ff68d754b210e466af801be86255bb898
Ingo Albrecht <commit@promovicz.org>
2021-11-22 20:32:26 +0100

    Change tags

--- build-simh.sh
+++ build-simh.sh
@@ -5,13 +5,13 @@ IMAGES="simh-pdp10 simh-pdp11 simh-vax650 simh-vax780 simh-vax860"
 time docker build . \
      --network host \
      -f Dockerfile.simh-base-alpine \
-     -t "simh-base:alpine-latest" \
+     -t "simh-base:alpine" \
      "$@"
 
 for image in ${IMAGES}; do
     time docker build . \
 	 --network none \
 	 -f Dockerfile.${image}-alpine \
-	 -t "${image}:alpine" -t "${image}:alpine-latest"\
+	 -t "${image}:alpine" \
 	 "$@"
 done
