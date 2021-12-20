a4a4445f7e18dad2cf2280f35d0bcade7973bf42
Ingo Albrecht <commit@promovicz.org>
2021-12-20 19:22:26 +0100

    Improved simh

--- build-simh.sh
+++ build-simh.sh
@@ -2,16 +2,14 @@
 
 IMAGES="simh-pdp10 simh-pdp11 simh-vax650 simh-vax780 simh-vax860"
 
+#time docker build . \
+#     --network host \
+#     -f Dockerfile.simh-alpine \
+#     -t "simh:alpine" \
+#     "$@"
+
 time docker build . \
      --network host \
-     -f Dockerfile.simh-base-alpine \
-     -t "simh-base:alpine" \
+     -f Dockerfile.simh-debian \
+     -t "simh:debian" \
      "$@"
-
-for image in ${IMAGES}; do
-    time docker build . \
-	 --network none \
-	 -f Dockerfile.${image}-alpine \
-	 -t "${image}:alpine" \
-	 "$@"
-done
