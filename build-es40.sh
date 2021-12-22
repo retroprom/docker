35b141fbc4db9f2a4f28455e298677d4a1f57b57
Ingo Albrecht <commit@promovicz.org>
2021-12-22 06:39:26 +0100

    Tabify

--- build-es40.sh
+++ build-es40.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #time docker build . \
-#     --network host \
-#     -f Dockerfile.es40-alpine \
-#     -t "retro-es40:alpine" \
-#     "$@"
+#	--network host \
+#	-f Dockerfile.es40-alpine \
+#	-t "retro-es40:alpine" \
+#	"$@"
 
 time docker build . \
-     --network host \
-     -f Dockerfile.es40-debian \
-     -t "retro-es40:debian" \
-     "$@"
+	--network host \
+	-f Dockerfile.es40-debian \
+	-t "retro-es40:debian" \
+	"$@"
