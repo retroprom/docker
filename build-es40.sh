ddd5dc91f740e46ddfc39bb59a4af5543efcf72c
Ingo Albrecht <commit@promovicz.org>
2021-12-22 05:12:07 +0100

    Added experimental build of es40

---
+++ build-es40.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#time docker build . \
+#     --network host \
+#     -f Dockerfile.es40-alpine \
+#     -t "retro-es40:alpine" \
+#     "$@"
+
+time docker build . \
+     --network host \
+     -f Dockerfile.es40-debian \
+     -t "retro-es40:debian" \
+     "$@"
