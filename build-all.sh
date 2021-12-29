1b360026177dda0962e71b28ad088b162ad2afd0
Ingo Albrecht <commit@promovicz.org>
2021-12-29 23:15:35 +0100

    Moved emulators to a subdirectory

--- build-all.sh
+++ build-all.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-# simple packages first
-./build-basilisk.sh
-./build-bochs.sh
-./build-qemu.sh
+cd emulators
+./build-all.sh
+cd ..
 
-# then the fast building ones
-./build-dynamips.sh
-./build-klh10.sh
-
-# finally the slow-building ones
-./build-gxemul.sh
-./build-hercules.sh
-./build-simh.sh
-./build-simx.sh
---
+++ emulators/build-all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# simple packages first
+./build-basilisk.sh
+./build-bochs.sh
+./build-qemu.sh
+
+# then the fast building ones
+./build-dynamips.sh
+./build-klh10.sh
+
+# finally the slow-building ones
+./build-gxemul.sh
+./build-hercules.sh
+./build-simh.sh
+./build-simx.sh
