1781239b982e48d52d5c47fbcdf1557a406f1d01
Ingo Albrecht <commit@promovicz.org>
2022-11-26 03:23:28 +0100

    simh unix updates

--- system/simh/build.sh
+++ system/simh/build.sh
@@ -2,4 +2,4 @@
 
 set -e 
 
-docker build unix-v7 "$@"
+docker build unix-v7 -f Dockerfile -t retroprom/unix-v7 "$@"
--- system/simh/download.sh
+++ system/simh/download.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-wget -c -O unix-v5/uv5swre.zip http://simh.trailing-edge.com/kits/uv5swre.zip
-wget -c -O unix-v6/uv6swre.zip http://simh.trailing-edge.com/kits/uv6swre.zip
-wget -c -O unix-v7/uv7swre.zip http://simh.trailing-edge.com/kits/uv7swre.zip
+mkdir -p download
+
+wget -c -O download/uv5swre.zip http://simh.trailing-edge.com/kits/uv5swre.zip
+wget -c -O download/uv6swre.zip http://simh.trailing-edge.com/kits/uv6swre.zip
+wget -c -O download/uv7swre.zip http://simh.trailing-edge.com/kits/uv7swre.zip
 
 sha256sum -c SHA256SUMS
 
+pushd unix-v5; unzip -u ../download/uv5swre.zip; popd
+pushd unix-v6; unzip -u ../download/uv6swre.zip; popd
+pushd unix-v7; unzip -u ../download/uv7swre.zip; popd
