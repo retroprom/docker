7e22cee7986aa11316eab3264952e408aa781a50
Ingo Albrecht <commit@promovicz.org>
2022-11-26 01:00:42 +0100

    Cisco updates

--- cisco/build.sh
+++ cisco/build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 time docker build \
-	images/ios-7200 \
+	images/ios-1700 \
 	-f Dockerfile \
-	-t retroprom/cisco-7200:latest \
-	--build-arg OPTIONS="-P 7200" \
-	--build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
+	-t retroprom/cisco-1720:latest \
+	--build-arg OPTIONS="-P 1700 -t 1720" \
+	--build-arg IMAGE="c1700-k9o3sy7-mz.123-23.bin" \
 	"$@"
+    
+time docker build \
+    images/ios-2691 \
+    -f Dockerfile \
+    -t retroprom/cisco-2691:latest \
+    --build-arg OPTIONS="-P 2691" \
+    --build-arg IMAGE="c2691-adventerprisek9_sna-mz.124-13b.bin" \
+    "$@"
+
+time docker build \
+    images/ios-3620 \
+    -f Dockerfile \
+    -t retroprom/cisco-3620:latest \
+    --build-arg OPTIONS="-P 3600 -t 3620" \
+    --build-arg IMAGE="c3620-ik9s-mz.122-40a.bin" \
+    "$@"
 
 time docker build \
 	images/ios-3640 \
@@ -22,5 +38,13 @@ time docker build \
 	-t retroprom/cisco-3660:latest \
 	--build-arg OPTIONS="-P 3600 -t 3660" \
 	--build-arg IMAGE="c3660-ik9o3s-mz.124-13b.bin" \
+	"$@"
+
+time docker build \
+	images/ios-7200 \
+	-f Dockerfile \
+	-t retroprom/cisco-7200:latest \
+	--build-arg OPTIONS="-P 7200" \
+	--build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
 	"$@"
 
