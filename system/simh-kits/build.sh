6c16d9ea2ebc8eab3ab4af84ec9a993986c80394
Ingo Albrecht <commit@promovicz.org>
2022-12-28 11:24:27 +0100

    simh: Various improvements

--- system/simh-kits/build.sh
+++ system/simh-kits/build.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-docker build pdp11-rsts-v7 -f Dockerfile -t retroprom/simh-pdp11-rsts-v7 "$@"
-docker build pdp11-rt11-v4 -f Dockerfile -t retroprom/simh-pdp11-rt11-v4 "$@"
-docker build pdp11-rt11-v5 -f Dockerfile -t retroprom/simh-pdp11-rt11-v5 "$@"
-docker build pdp11-unix-v5 -f Dockerfile -t retroprom/simh-pdp11-unix-v5 "$@"
-docker build pdp11-unix-v6 -f Dockerfile -t retroprom/simh-pdp11-unix-v6 "$@"
-docker build pdp11-unix-v7 -f Dockerfile -t retroprom/simh-pdp11-unix-v7 "$@"
+if ! [ -d download ]; then
+    ./download.sh
+fi
+
+./extract.sh
+
+docker build build/pdp11-rsts-v7 -f Dockerfile -t retroprom/simh-pdp11-rsts-v7 "$@"
+docker build build/pdp11-rt11-v4 -f Dockerfile -t retroprom/simh-pdp11-rt11-v4 "$@"
+docker build build/pdp11-rt11-v5 -f Dockerfile -t retroprom/simh-pdp11-rt11-v5 "$@"
+docker build build/pdp11-unix-v5 -f Dockerfile -t retroprom/simh-pdp11-unix-v5 "$@"
+docker build build/pdp11-unix-v6 -f Dockerfile -t retroprom/simh-pdp11-unix-v6 "$@"
+docker build build/pdp11-unix-v7 -f Dockerfile -t retroprom/simh-pdp11-unix-v7 "$@"
--- system/simh-kits/extract.sh
+++ system/simh-kits/extract.sh
@@ -2,14 +2,42 @@
 
 set -e
 
+ROOT=$PWD
+DIST=$PWD/download
+
 sha256sum -c SHA256SUMS
 
-pushd id32-unix-v6; unzip -u ../download/iu6swre.zip; popd
-pushd id32-unix-v7; unzip -u ../download/iu7swre.zip; popd
-pushd pdp11-rsts-v7; tar xfvz ../download/rstsv7gen.tar.Z; tar xfvz ../download/rstsv7swre.tar.Z; popd
-pushd pdp11-rt11-v4; tar xfvz ../download/rt11swre.tar.Z; popd
-pushd pdp11-rt11-v5; tar xfvz ../download/rtv53swre.tar.Z; popd
-pushd pdp11-unix-v5; unzip -u ../download/uv5swre.zip; popd
-pushd pdp11-unix-v6; unzip -u ../download/uv6swre.zip; popd
-pushd pdp11-unix-v7; unzip -u ../download/uv7swre.zip; popd
+rm -rf build
+
+enter() {
+    echo Extracting for $1...
+    mkdir -p $ROOT/build/$1
+    cp -r $ROOT/$1/* $ROOT/build/$1
+    cd $ROOT/build/$1
+}
+
+enter id32-unix-v6
+unzip -u $DIST/iu6swre.zip
+
+enter id32-unix-v7
+unzip -u $DIST/iu7swre.zip
+
+enter pdp11-rsts-v7
+tar xfvz $DIST/rstsv7gen.tar.Z
+tar xfvz $DIST/rstsv7swre.tar.Z
+
+enter pdp11-rt11-v4
+tar xfvz $DIST/rt11swre.tar.Z
+
+enter pdp11-rt11-v5;
+tar xfvz $DIST/rtv53swre.tar.Z
+
+enter pdp11-unix-v5;
+unzip -u $DIST/uv5swre.zip
+
+enter pdp11-unix-v6;
+unzip -u $DIST/uv6swre.zip
+
+enter pdp11-unix-v7;
+unzip -u $DIST/uv7swre.zip
 
