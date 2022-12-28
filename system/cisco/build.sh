86397b5a59002bb3bc267505a47bf4e5ef5afb9a
Ingo Albrecht <commit@promovicz.org>
2022-12-28 11:26:55 +0100

    cisco: Updates

--- system/cisco/build.sh
+++ system/cisco/build.sh
@@ -3,82 +3,42 @@
 time docker build \
 	images/ios-1700 \
 	-f Dockerfile \
-	-t retroprom/cisco-1720:latest \
+	-t retroprom/cisco-1700:latest \
 	--build-arg OPTIONS="-P 1700 -t 1720" \
 	--build-arg IMAGE="c1700-k9o3sy7-mz.123-23.bin" \
 	"$@"
 
 time docker build \
-    images/ios-2600 \
-    -f Dockerfile \
-    -t retroprom/cisco-2610:latest \
-    --build-arg OPTIONS="-P 2600 -t 2610 -r 64" \
-    --build-arg IMAGE="c2600-ik9o3s3-mz.123-23.bin" \
-    "$@"
+	images/ios-2600 \
+	-f Dockerfile \
+	-t retroprom/cisco-2600:latest \
+	--build-arg OPTIONS="-P 2600 -t 2620 -r 64" \
+	--build-arg IMAGE="c2600-ik9o3s3-mz.123-23.bin" \
+	"$@"
 
 time docker build \
-    images/ios-2600 \
-    -f Dockerfile \
-    -t retroprom/cisco-2611:latest \
-    --build-arg OPTIONS="-P 2600 -t 2611 -r 64" \
-    --build-arg IMAGE="c2600-ik9o3s3-mz.123-23.bin" \
-    "$@"
+	images/ios-2600xm \
+	-f Dockerfile \
+	-t retroprom/cisco-2600xm:latest \
+	--build-arg OPTIONS="-P 2600 -t 2650xm -r 128" \
+	--build-arg IMAGE="c2600-adventerprisek9-mz.124-17.bin" \
+	"$@"
 
 time docker build \
-    images/ios-2600 \
-    -f Dockerfile \
-    -t retroprom/cisco-2620:latest \
-    --build-arg OPTIONS="-P 2600 -t 2620 -r 64" \
-    --build-arg IMAGE="c2600-ik9o3s3-mz.123-23.bin" \
-    "$@"
+	images/ios-2691 \
+	-f Dockerfile \
+	-t retroprom/cisco-2691:latest \
+	--build-arg OPTIONS="-P 2691 -r 128" \
+	--build-arg IMAGE="c2691-adventerprisek9_sna-mz.124-13b.bin" \
+	"$@"
 
 time docker build \
-    images/ios-2600 \
-    -f Dockerfile \
-    -t retroprom/cisco-2621:latest \
-    --build-arg OPTIONS="-P 2600 -t 2621 -r 64" \
-    --build-arg IMAGE="c2600-ik9o3s3-mz.123-23.bin" \
-    "$@"
-
-time docker build \
-    images/ios-2600xm \
-    -f Dockerfile \
-    -t retroprom/cisco-2610xm:latest \
-    --build-arg OPTIONS="-P 2600 -t 2610xm -r 128" \
-    --build-arg IMAGE="c2600-adventerprisek9-mz.124-17.bin" \
-    "$@"
-
-time docker build \
-    images/ios-2600xm \
-    -f Dockerfile \
-    -t retroprom/cisco-2620xm:latest \
-    --build-arg OPTIONS="-P 2600 -t 2620xm -r 128" \
-    --build-arg IMAGE="c2600-adventerprisek9-mz.124-17.bin" \
-    "$@"
-
-time docker build \
-    images/ios-2600xm \
-    -f Dockerfile \
-    -t retroprom/cisco-2650xm:latest \
-    --build-arg OPTIONS="-P 2600 -t 2650xm -r 128" \
-    --build-arg IMAGE="c2600-adventerprisek9-mz.124-17.bin" \
-    "$@"
-    
-time docker build \
-    images/ios-2691 \
-    -f Dockerfile \
-    -t retroprom/cisco-2691:latest \
-    --build-arg OPTIONS="-P 2691 -r 128" \
-    --build-arg IMAGE="c2691-adventerprisek9_sna-mz.124-13b.bin" \
-    "$@"
-
-time docker build \
-    images/ios-3620 \
-    -f Dockerfile \
-    -t retroprom/cisco-3620:latest \
-    --build-arg OPTIONS="-P 3600 -t 3620 -r 64" \
-    --build-arg IMAGE="c3620-ik9s-mz.122-40a.bin" \
-    "$@"
+	images/ios-3620 \
+	-f Dockerfile \
+	-t retroprom/cisco-3620:latest \
+	--build-arg OPTIONS="-P 3600 -t 3620 -r 64" \
+	--build-arg IMAGE="c3620-ik9s-mz.122-40a.bin" \
+	"$@"
 
 time docker build \
 	images/ios-3640 \
@@ -97,57 +57,9 @@ time docker build \
 	"$@"
 
 time docker build \
-    images/ios-7200 \
-    -f Dockerfile \
-    -t retroprom/cisco-7200-100:latest \
-    --build-arg OPTIONS="-P 7200 -t npe-100 -r 128" \
-    --build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
-    "$@"
-
-time docker build \
-    images/ios-7200 \
-    -f Dockerfile \
-    -t retroprom/cisco-7200-150:latest \
-    --build-arg OPTIONS="-P 7200 -t npe-150 -r 128" \
-    --build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
-    "$@"
-
-time docker build \
-    images/ios-7200 \
-    -f Dockerfile \
-    -t retroprom/cisco-7200-175:latest \
-    --build-arg OPTIONS="-P 7200 -t npe-175 -r 128" \
-    --build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
-    "$@"
-
-time docker build \
-    images/ios-7200 \
-    -f Dockerfile \
-    -t retroprom/cisco-7200-200:latest \
-    --build-arg OPTIONS="-P 7200 -t npe-200 -r 128" \
-    --build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
-    "$@"
-
-time docker build \
-    images/ios-7200 \
-    -f Dockerfile \
-    -t retroprom/cisco-7200-225:latest \
-    --build-arg OPTIONS="-P 7200 -t npe-225 -r 128" \
-    --build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
-    "$@"
-
-time docker build \
-    images/ios-7200 \
-    -f Dockerfile \
-    -t retroprom/cisco-7200-300:latest \
-    --build-arg OPTIONS="-P 7200 -t npe-300 -r 128" \
-    --build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
-    "$@"
-
-time docker build \
 	images/ios-7200 \
 	-f Dockerfile \
-	-t retroprom/cisco-7200-400:latest \
+	-t retroprom/cisco-7200:latest \
 	--build-arg OPTIONS="-P 7200 -t npe-400 -r 128" \
 	--build-arg IMAGE="c7200-jk9s-mz.124-13b.bin" \
 	"$@"
