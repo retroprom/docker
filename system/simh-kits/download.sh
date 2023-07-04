9ec81a5bc0a78de2c895c7872a82edf832282ca1
Ingo Albrecht <commit@promovicz.org>
2023-07-04 20:12:32 +0200

    Comments

--- system/simh-kits/download.sh
+++ system/simh-kits/download.sh
@@ -8,15 +8,19 @@ get() {
 
 mkdir -p download
 
-get iu6swre.zip
-get iu7swre.zip
+# DEC RSTS/E
 get rstsv7gen.tar.Z
 get rstsv7swre.tar.Z
+# DEC RT-11
 get rt11swre.tar.Z
 get rtv53swre.tar.Z
+# PDP-11 UNIX (Caldera-licensed)
 get uv5swre.zip
 get uv6swre.zip
 get uv7swre.zip
+# Interdata UNIX
+get iu6swre.zip
+get iu7swre.zip
 
 sha256sum -c SHA256SUMS
 
