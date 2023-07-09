b87d54feb839ff7773ad5b55b0dd650f2b8ed0ab
Ingo Albrecht <commit@promovicz.org>
2023-07-09 04:24:10 +0200

    Rename top-level directories, script comments

--- build-emulators.sh
+++ build-emulators.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-set -e
+# Be strict
+set -euo pipefail
 
-ROOT=$PWD
+# Remember root directory
+ROOT="$PWD"
 
+# Configuration
 BASE="debian:bookworm"
 EMULATORS="craysim dynamips es40 gxemul hercules klh10 simh"
 
+# Build each emulator
 for emulator in ${EMULATORS}; do
+	# Run the build
 	docker build \
-		-f emulator/${emulator}/Dockerfile.debian \
+		-f emulators/${emulator}/Dockerfile.debian \
 		-t retroprom/${emulator}:latest \
 		--build-arg BASE=${BASE} \
 		"$@" \
-		emulator/${emulator}
+		emulators/${emulator}
 done
--- build-systems.sh
+++ build-systems.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-ROOT=$PWD
+ROOT="$PWD"
 
 SYSTEMS="klh10-panda simh-kits simh-os32 hercules-kits"
 
+# Build each system by calling its scripts
 for system in ${SYSTEMS}; do
-	if [ -x $ROOT/system/${system}/build.sh ]; then
-		cd $ROOT/system/${system}
-		if [ -x download.sh ]; then ./download.sh; fi
-		./build.sh "$@"
-	fi
+	# Change directory
+	cd $ROOT/systems/${system}
+	# Perform download if required
+	if [ -x download.sh ]; then ./download.sh; fi
+	# Perform build
+	./build.sh "$@"
 done
+
