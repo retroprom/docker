2c1666bf424ed8c5872099cd3e15873e29252450
Ingo Albrecht <commit@promovicz.org>
2023-07-11 03:44:13 +0200

    Working ITS build - but very slow

--- systems/its/build.sh
+++ systems/its/build.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-docker build . -f Dockerfile -t retroprom/its-klh10:latest "$@"
+docker build . -f Dockerfile \
+	-t retroprom/its-klh10:latest \
+	--build-arg ITS_TARGET=klh10 \
+	"$@"
+
+docker build . -f Dockerfile \
+	-t retroprom/its-simh:latest \
+	--build-arg ITS_TARGET=simh \
+	"$@"
 
--- systems/its/run.sh
+++ systems/its/run.sh
@@ -6,8 +6,5 @@
 # Be strict
 set -euo pipefail
 
-# Must run in build directory
-cd its
-
 # Execute the emulator
 exec ./start "${ITS_OPTIONS}"
